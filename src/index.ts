ab77f55ae7640f1114a6ef713e48d4b7a9128b05
fengchao <chao.feng9@pactera.com>
2019-11-20 11:31:43 +0800

    feat: 添加打包后全局方法

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { Map } from "./core/Map";
 
-var map = new Map()
+// 添加全局方法，打包后引入js可直接使用 window.warehouse()
+(<any>window).warehouse = (options?) => {
+  return new Map(options);
+}
+
+export class Warehouse {
+  map: Map;
+
+  constructor(options?) {
+    this.map = new Map(options);
+  }
+}
